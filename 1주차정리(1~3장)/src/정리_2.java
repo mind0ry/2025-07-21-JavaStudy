760f3e44560ba4e2926f5ec18446d56425e778e7
sist <sist@sist35>
2025-07-25 12:23:37 +0900

    2025-07-25

---
+++ 1주차정리(1~3장)/src/정리_2.java
@@ -0,0 +1,88 @@
+/*
+ * 	변수: 한개의 데이터값을 저장할 수 있는 메모리 공간의 이름
+ * 								  -------------
+ * 								  메모리는 주소/번지
+ * 										 ------
+ * 										 | 별칭
+ * 	1) 임시로 데이터값을 컴퓨터에 인식 => 재사용
+ * 	   변수 : 한개의 값
+ * 	   배열 : 같은 것 여러개 => 사과박수 , 배박스 
+ * 	   클래스 : 다른 것 여러개 => 종합과일세트
+ * 	   ---------------------------------- 메모리에 저장
+ * 	   File
+ * 	   ***RDBMS(오라클)
+ * 	   ---------------------------------- 영구적으로 저장이 가능
+ * 	   => 프로그램은 저장 (데이터 관리) => CRUD
+ * 		  
+ * 	2) 변수 => 읽기 / 쓰기(저장, 수정)
+ * 			  ------------------
+ * 			  | 기능 : setter / getter
+ * 	3) 메모리 크기 (데이터형)
+ * 		정수
+ * 			byte (1byte) 채팅 => byte, 파일업로드/파일 다운로드
+ * 			---------------
+ * 			int	 (4byte) = 일반 정수 => 가장 사용되는 데이터형
+ * 			long (8byte) = 통계 , 확률 , 빅데이터
+ * 			--------------- l(L)
+ * 		실수
+ * 			double (8byte) => 15자리(정밀도)
+ * 		문자
+ * 			char (2byte) => 0~65535
+ * 		논리
+ * 			boolean (1byte) => true/false만 저장 가능
+ * 	4) 변수 선언
+ * 		데이터형 변수명; => 메모리 저장 공간 확보
+ * 			    |
+ * 			  = 알파벳 , 한글로 시작
+ * 				  (대소문자 구분)
+ * 			  = 숫자는 사용이 가능 (앞에 사용 금지)
+ * 			  = 키워드는 사용 금지 (int , long ...)
+ * 			  = 특수문자 => (_ , $)
+ * 			  = 공백없이 사용
+ * 			  = 문자 길이 => 제한 (3~7)
+ * 				------------------- 가독성 (리팩토링)
+ * 			 	=> 코드 리뷰 / 디자인 패턴 / 공통 모듈
+ * 								| 8개
+ * 				=> 테스트 (Junit)
+ * 	5) 초기값 부여
+ * 		=> 변수선언 => 초기값이 없는 경우에는 사용이 불가능
+ * 		   byte => 0
+ * 		   int => 0
+ * 		   long => 0L
+ * 		   char => ' '
+ * 		   boolean => false
+ * 		   double => 0.0
+ * 		   String => ""
+ * 		   ====> 변수 => 필요시마다 변경해서 사용
+ * 		=> 명시적 초기화  => int a=10;
+ * 		=> 사용자 입력값 : Scanner / JOptionPane ...
+ * 		=> 임의로 데이터 추출 => random
+ * 
+ */
+public class 정리_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b1=Byte.MIN_VALUE;
+		byte b2=Byte.MAX_VALUE;
+		
+		int i1=Integer.MIN_VALUE;
+		int i2=Integer.MAX_VALUE;
+		
+		long l1=Long.MIN_VALUE;
+		long l2=Long.MAX_VALUE;
+		
+		double d1=Double.MIN_NORMAL;
+		double d2=Double.MAX_VALUE;
+		
+		char c1=Character.MIN_VALUE;
+		char c2=Character.MAX_VALUE;
+		
+		System.out.println("byte:"+b1+"~"+b2);
+		System.out.println("int:"+i1+"~"+i2);
+		System.out.println("long:"+l1+"~"+l2);
+		System.out.println("double:"+d1+"~"+d2);
+		System.out.println("char:"+0+"~"+65535);
+	}
+
+}
---
+++ 1주차정리(1~3장)/src/정리_3.java
@@ -0,0 +1,174 @@
+// 변수 / 연산자 / 제어문
+// 묶기
+/*
+ * 	변수 => 데이터를 저장 => 연산 처리
+ * 	---                 -------
+ * 						   |
+ * 						응용 : 제어문
+ * 	변수+연산자+제어문 
+ * 	------------- 메소드
+ * 	메소드 여러개 묶음
+ * 	------------- 클래스
+ * 	클래스 여러개 묶음
+ * 	------------- 패키지 => .jar
+ * 
+ * 	연산자 
+ * 		= 단항연산자
+ * 		  증감연산자 (++, --)
+ * 			전치연산자 : 먼저 증가 ==> ++a
+ * 			후치연산자 : 나중에 증가 ==> a++
+ * 			--------------------------- 공통점 : 1증가 , 1감소
+ * 		 	=> 출력시에도 적용
+ * 			int a=10;
+ * 			System.out.println(a++); 출력 => 증가
+ * 			System.out.println(++a);
+ * 
+ * 		  부정연산자 (!) : 부정
+ * 						boolean에서 사용이 가능
+ * 						true => false , false => true
+ * 						=> 조건문 
+ * 						   ----- 1) 삼항연산자
+ * 								 2) if ..
+ * 		  형변환연산자 (데이터형)
+ * 		= 이항연산자
+ * 		  산술연산자 (+,-,*,/,%)
+ * 			=> 자동형변환
+ * 				=> 큰 데이터형으로 변경 후 연산
+ * 				   --------------------- # 연산은 같은 데이터형끼리만 연산
+ * 					예)
+ * 						double + int
+ * 								 --- double로 변경
+ * 						------------- 연산
+ * 						10.5+10
+ * 							 --- 자동으로 10.0으로 변경
+ * 						char + int
+ * 						----
+ * 						 int
+ * 						------------- 연산
+ * 				=> int 이하는 연산 시에 결과값이 int
+ * 				   ------- char / byte
+ * 
+ * 		  비교연산자 (==, !=, <, >,<=, >=)
+ * 			=> 숫자만 비교가 가능 (실수, 정수, 문자(숫자취급))
+ * 			=> 예외 (boolean ==, !=)
+ * 			=> 조건문 / 반복문에서 주로 사용
+ * 			   ----- 삼항연산자
+ * 			=> 결과값은 boolean (true/false)
+ * 			=> 문자열 비교
+ * 				=> equals 사용
+ * 		  논리연산자 (&& , ||)
+ * 			(조건) && (조건)  => true/false
+ * 			효율적인 연산 => 왼쪽에 있는 조건이 false면 오른쪽에 있는 조건은 수행하지 않는다
+ * 			=> 사용 : 범위, 기간 포함 여부 확인
+ * 						 ---- 예약이 가능
+ * 					 --- 페이징 / 점수
+ * 					 --- 오라클 : Between ~ AND 
+ * 			(조건) || (조건) => 조건 : true/false
+ * 			효율적인 연산 => 왼쪽 조건값이 true면 오른쪽은 수행하지 않는다
+ * 			사용 => 범위, 기간외 .. 잘못된 입력인 경우 => 오류 처리
+ * 			
+ * 			알파벳 => 소문자
+ * 			c>='a' && c<='z'
+ * 			c>='A' && c<='Z'
+ * 
+ * 			범위외
+ * 			a<0 || a>100
+ * 
+ * 			세자리 정수 입력
+ * 			a>=100 && a<1000 => a<=999
+ * 		  대입연산자 (= , += , -=)
+ * 			= 대입
+ * 			+= , -= : 복합 대입 연산자
+ * 			a+=1 ==> a=a+1 => 1더하기
+ * 			a+=2 ==> a=a+2 => 2더하기
+ * 			=> 결과값 (연산자) , 초기값 대입 , 변경된 값
+ * 
+ * 		= 삼항연산자 ((조건)? 값1:값2)
+ * 					 | true => 값1
+ * 					 | false => 값2
+ * 			=> 소스가 간결 : 게임 / 웹 = 삼항연산자
+ * 				if ~ else => 일반 Application
+ * 		
+ */
+public class 정리_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=10;
+		int z;
+		
+		x++;
+		++x;
+		System.out.println("x="+x); //12
+		//전치 / 후치 => 무조건 1개 증가
+		System.out.println("=====");
+		y--;
+		--y;
+		System.out.println("y="+y);
+		System.out.println("=====");
+		
+		z=x++;
+		/*
+		 * 	z = x ++
+		 *  ----- z=x => z=12
+		 *  	  ---
+		 */
+		System.out.println("z="+z);
+		System.out.println("=====");
+		z=++x;
+		System.out.println("z="+z);
+		System.out.println("x="+x);
+		
+		z= ++x + y++;
+		//x=14 , y=8
+		System.out.println("z="+z);
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		
+		System.out.println("=====");
+		boolean bCheck=false;
+		System.out.println(bCheck);
+		System.out.println(!bCheck);
+		// 반대로 데이터를 읽는 경우
+		System.out.println("=====");
+		System.out.println(65.0);
+		System.out.println((int)65.0);
+		System.out.println((char)65.0);
+		System.out.println((double)'A');
+		System.out.println((double)65);
+		/*
+		 * 		=====================> up
+		 * 	byte < char < int < long < double
+		 * 		<===================== down
+		 * 	숫자 관련만 가능 (boolean은 제외)
+		 * 	큰데이터 = 작은 데이터형 => 자동으로 변경
+		 * 	
+		 * 	double d=10
+		 * 			 -- 10.0
+		 * 	int i = 'A'
+		 * 			-- 65
+		 * 	작은 데이터형 = 큰 데이터형
+		 * 				--------- 작은 데이터형으로 변경
+		 * 	int i=10.5
+		 * 		  ---- 오류 발생
+		 * 	int i=(int)10.5 => 10
+		 * 
+		 * 	예외)
+		 * 		char c=65; => 선언과 동시에 초기값으로만 가능
+		 * 
+		 * 
+		 */
+//		int i=65;
+//		char c=(char)i;
+		
+		char c1='A';
+		char c2='B';
+		int c3=c1+c2; // int / long / double
+		
+		byte b1=10;
+		byte b2=20;
+		double b3=b1+b2;
+	}
+
+}
