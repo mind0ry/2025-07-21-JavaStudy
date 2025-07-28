10cc16fc44b93c333e00a3e7d1361e6f37bf2913
mind0ry <mindory144@google.com>
2025-07-29 08:33:36 +0900

    2025-07-28

---
+++ Chapter04_제어문(조건문,선택문문제)/src/문제정리.java
@@ -0,0 +1,25 @@
+/*
+ * 
+ * 	1. 형식
+ * 		단일 조건문 
+ * 		if (조건문) => 검색 , 로그인 , 아이디 찾기
+ * 					  비밀번호 찾기 , 아이디 중복체크
+ * 					  아이디저장 , 자동 로그인
+ * 		   ------ 부정연산자 , 비교연산자 , 논리연산자
+ * 		   ------ equals(같다) , contains(포함)
+ * 		   ------ startsWith(시작문자) , endsWith(끝문자열)
+ * 		   ------ !equals()
+ *  	{
+ *  		실행문장	=> 조건문이 TRUE일 경우에만 실행
+ *  					FALSE면 건너뛴다
+ *  	}
+ *  
+ */
+public class 문제정리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
