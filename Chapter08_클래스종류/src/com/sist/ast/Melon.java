ebcd834e7791c0bb41bbd230375af8b2712eb5db
KIM MINSEOK <mindory144@gmail.com>
2025-08-18 17:50:43 +0900

    2025-08-18

---
+++ Chapter08_클래스종류/src/com/sist/ast/Melon.java
@@ -0,0 +1,11 @@
+package com.sist.ast;
+
+public class Melon extends MusicClass {
+
+	@Override
+	public void musicAllData() {
+		// TODO Auto-generated method stub
+		System.out.println("Melon 크롤링 완료");
+	}
+	
+}
