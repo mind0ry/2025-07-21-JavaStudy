9b4b559339078c0003f40ff5ff0f196b12bba8c5
KIM MINSEOK <mindory144@gmail.com>
2025-08-21 17:45:38 +0900

    2025-08-21

---
+++ Chapter12_라이브러리(java.lang)/src/com/sist/lang/라이브러리_8.java
@@ -0,0 +1,72 @@
+package com.sist.lang;
+/*
+ * 	Wrapper 
+ * 		기본형을 클래스화
+ * 		정수형
+ * 			byte ==> Byte
+ * 			short ==> Short
+ * 			***int ==> Integer
+ * 			***long ==> Long
+ * 		문자형
+ * 			***char ==> Character
+ * 		실수형
+ * 			float ==> Float
+ * 			double ==> Double
+ * 		논리형
+ * 			boolean ==> Boolean
+ * 		문자열형
+ * 			char[] ==> String
+ * 
+ * 		=> 양쪽 호환
+ * 			오토박싱 / 언박싱 => 한개만 parseXxx()
+ * 			  |		  | 
+ * 					기본형에 클래스 객체 대입
+ * 			클래스의 값 대입
+ * 
+ * 		Integer ii=10;
+ * 		int i=ii;
+ * 			String	  String => 정수 변경 Integer.parseInt("10") => 10
+ * 		자바 =====> 서버 =====> 클라이언트
+ * 			10			
+ * 			String.valueOf(10)
+ * 				=> "10"
+ * 			=> 문자열 변경
+ * 			write(String s)
+ * 			String read()
+ * 
+ * 			정수 : Integer.parseInt("10")
+ * 			실수 : Double.parseDouble("10.5")
+ * 			논리 : Boolean.parseBoolean("true") // 체크박스
+ * 
+ * 			*** 웹/윈도우 => 데이터형 개념이 없다
+ * 							Only String
+ * 			입력창
+ * 				JTextField
+ * 				 ----
+ * 				<input type=text>
+ * 				-----------------
+ * 
+ * 				public String boardList(HttpServletRequest req)
+ * 				{
+ * 					String page=req.getParameter("page")
+ * 				}
+ * 
+ * 				p bublic String boardList(int page)
+ * 				{
+ * 
+ * 				}
+ */
+import java.util.*;
+public class 라이브러리_8 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Integer ii=new Integer(10);
+		//int i=ii.intValue();
+		int a=100;
+		System.out.println(Integer.toBinaryString(a));
+		System.out.println(Integer.toOctalString(a)); 
+		System.out.println(Integer.toHexString(a));
+		
+	}
+
+}
---
+++ Chapter12_라이브러리(java.lang)/src/com/sist/lang/라이브러리_9.java
@@ -0,0 +1,76 @@
+package com.sist.lang;
+import java.text.*;
+import java.util.Date;
+public class 라이브러리_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int won=1000000000;
+//		DecimalFormat df=new DecimalFormat("#,###,###,###");
+//		System.out.println(df.format(won));
+		Date date=new Date();
+		System.out.println(date);
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+		/*
+		 * 	yyyy / yy
+		 * 	MM   / M
+		 * 	dd   / d
+		 * 	------------
+		 * 	hh	/ h
+		 * 	mm	/ m
+		 * 	ss	/ s
+		 * 
+		 */
+		System.out.println(sdf.format(date));
+		int no=1;
+		String name="홍길동";
+		String sex="남자";
+		String address="서울";
+		String phone="010-1111-1111";
+		int age=30;
+		String content="홍길동입니다";
+		
+		// 오라클 연동
+		
+		String sql = "INSERT INTO member VALUES("
+	            + no + ",'" 
+	            + name + "','" 
+	            + sex + "','" 
+	            + address + "','" 
+	            + phone + "'," 
+	            + age + ")";
+		System.out.println(sql);
+		
+		sql="INSERT INTO member VALUES("
+				+"{0},''{1}'',''{2}'',''{3}'',''{4}'',{5})";
+		Object[] obj= {no,name,sex,address,phone,age};
+		System.out.println(MessageFormat.format(sql, obj));
+		
+		double[] limit= {59,60,70,80,90};
+		// 				0~59 60~69 70~79 80~89 90이상
+		String[] grade= {"F","D","C","B","A"};
+		int[] score=new int[10];
+		for(int i=0;i<score.length;i++) {
+			score[i]=(int)(Math.random()*101);
+		};
+		
+		ChoiceFormat cf=new ChoiceFormat(limit,grade);
+		for(int i=0;i<score.length;i++) {
+			System.out.println(score[i]+":"+cf.format(score[i]));
+		}
+		
+		/*
+		 * 	문자함수 / 숫자함수 / 날짜함수 / 변환함수 / 기타함수
+		 * 	  |		   |		|		|		 |
+		 * 	String	  Math	 Calendar Format	switch , if
+		 * 	SimpleDateFormat : 날짜 변경 => 날짜함수 (오라클)
+		 * 	DecimalFormat : 숫자변환 => 숫자함수
+		 * 	MessageFormat : SQL문장 사용
+		 * 	---------------------------
+		 * 	=> 웹 : SQL (Back-End : 90%)
+		 * 	--------------------------- 
+		 * 	
+		 */
+	}
+
+}
